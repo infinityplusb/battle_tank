daf66749cbfdc79760682711ef7c16ab1381a42b
Unknown <brian@infinityplusb.com>
2018-05-08 22:50:32 +1000

    Lecture 131 - Added Tick to Player Controller
    Added a C++ Tick Function to the Player Controller
    Added a pseudocode for aiming the crosshairs

--- battle_tank/Source/battle_tank/Private/TankPlayerController.cpp
+++ battle_tank/Source/battle_tank/Private/TankPlayerController.cpp
@@ -3,6 +3,7 @@
 
 #include "TankPlayerController.h"
 
+
 void ATankPlayerController::BeginPlay()
 {
     Super::BeginPlay();
@@ -19,8 +20,27 @@ void ATankPlayerController::BeginPlay()
     //UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
 
+// Tick
+// Called every frame
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+    AimTowardsCrosshair();    
+//    UE_LOG(LogTemp, Warning, TEXT("PlayerController tick!"));
+
+}
+
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair() 
+{
+    if (!GetControlledTank()) { return; }
+
+    // Get World location of linetrace through crosshair
+    // if it hits the landscape
+        // tell the tank to aim at that point
+}
--- battle_tank/Source/battle_tank/Public/TankPlayerController.h
+++ battle_tank/Source/battle_tank/Public/TankPlayerController.h
@@ -19,5 +19,11 @@ public:
 	ATank* GetControlledTank() const;
 	
 	virtual void BeginPlay() override ; // the virtual here is redundant
-	
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that a shot would hit where 
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
 };
