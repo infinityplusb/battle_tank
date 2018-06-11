97e604cb8471313c4c85422274089e35c5726f1d
Unknown <brian@infinityplusb.com>
2018-06-11 20:20:10 +1000

    Lecture 165 - Added Turn Left and Right

--- battle_tank/Source/battle_tank/Private/TankMovementComponent.cpp
+++ battle_tank/Source/battle_tank/Private/TankMovementComponent.cpp
@@ -7,19 +7,23 @@
 
 void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-    if(!LeftTrackToSet || !RightTrackToSet) {return; }
-
     LeftTrack = LeftTrackToSet ;
     RightTrack = RightTrackToSet ;
 }
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
-    UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
-
+    // UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+    if(!LeftTrack || !RightTrack) {return; }
     LeftTrack->SetThrottle(Throw);
-    RightTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(Throw); 
 }
    
-
+void UTankMovementComponent::IntendTurnRight(float Throw)
+{
+    // UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
+    if(!LeftTrack || !RightTrack) {return; }
+    LeftTrack->SetThrottle(Throw);
+    RightTrack->SetThrottle(-Throw);
+}
 
--- battle_tank/Source/battle_tank/Public/TankMovementComponent.h
+++ battle_tank/Source/battle_tank/Public/TankMovementComponent.h
@@ -23,6 +23,12 @@ public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void IntendMoveForward(float Throw) ;
 	
+	// Throw is how far back the controller is moved
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void IntendTurnRight(float Throw) ;
+	
+
+
 private:
 	UTankTrack* LeftTrack = nullptr ; 	
 	UTankTrack* RightTrack = nullptr ;
